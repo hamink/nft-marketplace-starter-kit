fc648afc4ef8f7b973a11ead434b96b35f624e26
hamink <hamin@atomrigs.io>
2022-06-06 19:15:07 -0600

    section 14

--- src/contracts/ERC721Enumerable.sol
+++ src/contracts/ERC721Enumerable.sol
@@ -9,20 +9,37 @@ contract ERC721Enumerable is ERC721 {
 
     mapping(uint256 => uint256) private _allTokenIndex;
     mapping(uint256 => uint256) private _ownedTokensIndex;
-    mapping(address => uint256[]) private _ownedToken;
+    mapping(address => uint256[]) private _ownedTokens;
 
     function totalSupply() public view returns (uint256) {
         return _allTokens.length;
     }
 
+    function tokenByIndex(uint256 index) public view returns (uint256) {
+        require(index < totalSupply(), 'global index is out of bounds');
+        return _allTokens[index];
+    }
+
+    function tokenOfOwnerByIndex(address owner, uint256 index) public view returns (uint256) {
+        require(index < balanceOf(owner), 'owner index is out of bounds');
+        return _ownedTokens[owner][index];
+    }
+
     function _mint(address to, uint256 tokenId) internal override(ERC721) {
         super._mint(to, tokenId);
 
-        _addTokensToTotalSupply(tokenId);
+        _addTokensToAllTokenEnumeration(tokenId);
+        _addTokensToOwnerEnumeration(to, tokenId);
     }
 
-    function _addTokensToTotalSupply(uint256 tokenId) private {
+    function _addTokensToAllTokenEnumeration(uint256 tokenId) private {
+        _allTokenIndex[tokenId] = _allTokens.length;
         _allTokens.push(tokenId);
+    }
+
+    function _addTokensToOwnerEnumeration(address to, uint256 tokenId) private {
+        _ownedTokens[to].push(tokenId);
+        _ownedTokensIndex[tokenId] = _ownedTokens[to].length;
     }
 
 }
